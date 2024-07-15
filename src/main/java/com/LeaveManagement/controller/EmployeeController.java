07d6c0ea7f6189f5e19c0f7ed0e7fd86eb27d1d2
nadaakoubri <nadaakoubri@gmail.com>
2024-07-15 14:23:00 +0200

    Merge remote-tracking branch 'origin/Backend' into Backend

--- src/main/java/com/LeaveManagement/controller/EmployeeController.java
+++ src/main/java/com/LeaveManagement/controller/EmployeeController.java
@@ -19,29 +19,29 @@ public class EmployeeController {
     @Autowired
     private EmployeeImp employeeImp;
 
-    @PostMapping(path = "/saveEmp")
+    @PostMapping(path = "/save")
     public  Long saveEmployee(@RequestBody Employees employees){
 
         Long id =employeeService.addEmployee(employees);
         return id;
 
     }
-    @GetMapping(path="/getEmployee")
+    @GetMapping(path="/getAll")
     public List<Employees> getAllEmployee(){
         return  employeeService.getAllEmployees();
     }
 
-    @GetMapping(path="/getEmployeeById/{Id}")
+    @GetMapping(path="/getById/{Id}")
     public Employees getEmployeeById(@PathVariable Long Id){
         return employeeService.GetEmployeeById(Id);
     }
-    @PutMapping(path = "/updateEmp/{id}")
+    @PutMapping(path = "/update/{id}")
     public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody Employees employee) {
         employeeService.updateEmployee(id,employee);
         return ResponseEntity.ok("Employee updated successfully");
     }
 
-    @DeleteMapping(path = "/deleteEmp/{id}")
+    @DeleteMapping(path = "/delete/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
         return ResponseEntity.ok("Employee deleted successfully");
